0c2aa47caca8d9d190289781e664d38cfa949957
Ruby Oz <rubyozgewu@gmail.com>
2022-06-29 13:41:05 +0800

    test

---
+++ template.js
@@ -0,0 +1,27 @@
+export default {
+  type: 'bubble',
+  body: {
+    type: 'box',
+    layout: 'vertical',
+    contents: [
+      {
+        type: 'text',
+        text: 'Brown Cafe',
+        weight: 'bold',
+        size: 'xl'
+      },
+      {
+        type: 'text',
+        text: 'dishes'
+      },
+      {
+        type: 'text',
+        text: 'address'
+      },
+      {
+        type: 'text',
+        text: 'tel'
+      }
+    ]
+  }
+}
